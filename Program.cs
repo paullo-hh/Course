6070d8781635da1024fd1eb871004df1968a3cff
Paulo H. <contacthenry96@gmail.com>
2024-08-26 17:12:08 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.IO.Pipes;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            string nome = "Maria";
+            int idade = 32;
+            float saldo = 10.35784f;
+
+            Console.WriteLine("{0} tem {1} anos e tem um saldo em conta de R$ {2:F2}", nome, idade, saldo);
+            Console.WriteLine($"{nome} tem {idade} anos e tem um saldo em conta de R$ {saldo:F2}");
+            Console.WriteLine(nome + " tem " + idade + " anos e tem um saldo em conta de R$ " + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais ");
+        }
+    }
+}
